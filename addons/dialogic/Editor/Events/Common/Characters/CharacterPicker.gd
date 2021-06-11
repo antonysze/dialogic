8ab01d97a7ac50bf51a8e8ae5dc79fed23ea2186
antonysze <antonyszex@gmail.com>
2021-06-11 13:42:18 +0800

    Add default option to character picker

--- addons/dialogic/Editor/Events/Common/Characters/CharacterPicker.gd
+++ addons/dialogic/Editor/Events/Common/Characters/CharacterPicker.gd
@@ -3,6 +3,9 @@ extends HBoxContainer
 
 signal character_selected(data)
 
+const DEFULTA_COLOR := Color('#FFFFFF')
+var default_option_name := '[Character]'
+var default_option_metadata := {}
 
 func _ready():
 	$Dropdown.get_popup().connect("index_pressed", self, '_on_character_selected')
@@ -11,8 +14,8 @@ func _ready():
 func _on_Dropdown_about_to_show():
 	var popup = $Dropdown.get_popup()
 	popup.clear()
-	popup.add_item('[Character]')
-	popup.set_item_metadata(0, {})
+	popup.add_item(default_option_name)
+	popup.set_item_metadata(0, default_option_metadata)
 	
 	var index = 1
 	for c in DialogicUtil.get_sorted_character_list():
@@ -24,7 +27,7 @@ func _on_Dropdown_about_to_show():
 func _on_character_selected(index: int):
 	var metadata = $Dropdown.get_popup().get_item_metadata(index)
 	if index == 0:
-		set_data('[Character]', Color('#FFFFFF'))
+		set_data(default_option_name, DEFULTA_COLOR)
 	else:
 		set_data($Dropdown.get_popup().get_item_text(index), metadata['color'])
 	emit_signal('character_selected', metadata)
@@ -32,14 +35,17 @@ func _on_character_selected(index: int):
 
 
 func set_data_by_file(file_name: String):
-	# This method is used when you don't know the character's color
-	var character = DialogicResources.get_character_json(file_name)
-	set_data(character['name'], Color(character['color']))
+	if file_name != '' && file_name != default_option_name:
+		# This method is used when you don't know the character's color
+		var character = DialogicResources.get_character_json(file_name)
+		set_data(character['name'], Color(character['color']))
+	else:
+		set_data(default_option_name, DEFULTA_COLOR)
 
 
-func set_data(text: String, color: Color = Color('#FFFFFF')):
+func set_data(text: String, color: Color = DEFULTA_COLOR):
 	if not text.empty():
 		$Dropdown.text = text
 	else:
-		$Dropdown.text = '[Character]'
+		$Dropdown.text = default_option_name
 	$Icon.set("self_modulate", Color(color))
